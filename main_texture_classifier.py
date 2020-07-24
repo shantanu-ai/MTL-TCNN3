1e6bdb8e38f28298dc91594114aeeb90007db58b
Shantanu Ghosh <beingshantanu2406@gmail.com>
2020-07-23 22:44:56 -0400

    updated main

--- main_texture_classifier.py
+++ main_texture_classifier.py
@@ -2,7 +2,7 @@ from Texture_Classification_Deep import Texture_Classification_Deep
 
 
 def texture_classifier_BL():
-    # Texture_Classification_Deep.DTD_Train_test()
+    Texture_Classification_Deep.DTD_Train_test()
     Texture_Classification_Deep.kth_Train_test()
     # Texture_Classification_Deep.uiuc_Train_test()
     # Texture_Classification_Deep.kylberg_Train_test()
